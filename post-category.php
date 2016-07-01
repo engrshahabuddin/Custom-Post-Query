9208075b891e2730af4fc5ca1d9f2b119dbd5345
MD. SHAHAB UDDIN <support@devsit.com>
2016-07-01 10:40:44 +0600

    Update and rename post -category.php to post-category.php

---
+++ post-category.php
@@ -0,0 +1,10 @@
+<?php
+	global $post; 
+	$category = get_the_category($post->ID); 
+	$categoryName= $category[0]->name; 
+	echo $categoryName; 
+	// Get the ID of a given category
+	$category_id = get_cat_ID( $categoryName );
+	$category_link = get_category_link( $category_id );
+	echo $category_link;
+?>
